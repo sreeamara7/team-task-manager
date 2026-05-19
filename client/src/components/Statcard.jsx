18e348cfb2159fbf2907a49abf8bc70fe0ead318
Amara Sree <amarasree6@gmail.com>
2026-05-19 18:18:04 +0530

    fix statcard component

--- client/src/components/Statcard.jsx
+++ client/src/components/Statcard.jsx
@@ -3,9 +3,15 @@ import React from "react";
 const StatCard = ({ title, value }) => {
   return (
     <div className="card">
-      <h4 style={{ color: "#94a3b8", marginBottom: "10px" }}>
+      <h4
+        style={{
+          color: "#94a3b8",
+          marginBottom: "10px",
+        }}
+      >
         {title}
       </h4>
+
       <h1>{value}</h1>
     </div>
   );
